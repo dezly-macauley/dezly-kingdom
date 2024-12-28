93e42faae150d6d8f0f912d9583c589e0db930d2
dezly-macauley <dezlymacauley@proton.me>
2024-12-28 19:42:32 +0200

    Added guide for creating modules

---
+++ configs/neovim/nvim/lua-syntax-guide/07_if_statements.lua
@@ -0,0 +1,9 @@
+local score = 2
+
+if score > 10 then
+    print("The score is greater than 10")
+elseif score == 10 then
+    print("The score is exactly 10")
+else
+    print("The score is less than 10")
+end
