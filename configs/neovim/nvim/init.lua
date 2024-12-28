2174629665b35a6935dfd4649f3baabeceef6585
dezly-macauley <dezlymacauley@proton.me>
2024-12-28 15:16:29 +0200

    Added guide on how to apply Neovim settings to specific filetypes

--- configs/neovim/nvim/init.lua
+++ configs/neovim/nvim/init.lua
@@ -1,3 +1,41 @@
+-- SECTION: Neovim Configuration File Structure
+
+-- NOTE: init.lua
+-- When you open Neovim, it will look inside the `nvim` directory 
+-- for a file specifically called `init.lua` and it will execute 
+-- everything in the file starting from the top to the bottom, in order.
+
+-- NOTE: `after/ftplugin/name_of_file_type.lua`
+-- This a specific file path in Neovim. Do not rename it.
+-- After Neovim had loaded all the settings from in init.lua, 
+-- it will then look for a directory inside the `nvim` directory 
+-- called `after/ftplugin`, 
+-- and check if it needs to overwrite any settings for specific file types
+
+-- E.g. Let's say that in `init.lua` I have 4 space indentation for all 
+-- the languages but for `TypeScript` I want 2 space indentation.
+
+-- If you are not sure what the filetype of a file is,
+-- open the file in Neovim and then run this command
+-- `:echo &filetype`
+
+-- Then create a file that will automatically apply certain settings when
+-- you open a TypeScript file:
+-- after/ftplugin/typescript.lua (Make sure it ends with .lua)
+
+-- Add this to the typescript.lua file:
+-- vim.opt_local.shiftwidth = 2
+
+-- NOTE: vim.opt_local means 
+-- So normally vim.opt_local means 
+-- "only apply these settings to this current file".
+-- This is to ensure that these setting are not applied 
+-- to everything in the editor. 
+
+-- And when used with the after/ftplugin structure Neovim 
+-- will apply this to all TypeScript files. 
+
+-------------------------------------------------------------------------------
 -- SECTION: Using Neovim
 
 -- To use Tutor
@@ -144,5 +182,11 @@ greetKing()
 
 -- `%` means `this current file`
 -- :so %
+
+-------------------------------------------------------------------------------
+-- SECTION: Neovim Options
+
+-- Four space indentation
+vim.opt.shiftwidth = 4
 
 -------------------------------------------------------------------------------
